3cbb0b499aad432f4833bc5dd289ce3bdb8d55db
ngabor0508 <ngabor0508@gmail.com>
2022-09-27 20:14:26 +0200

    Add project files.

---
+++ SZTF1HF0017_MK8A80/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace SZTF1HF0017_MK8A80
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            long[] sajatMeresek = new long[5];
+            long[] baratunkMeresei = new long[5];
+
+            for (int i = 0; i < sajatMeresek.Length; i++)
+            {
+                Console.WriteLine("Add meg a bemeneti értékeket: ");
+                sajatMeresek[i] = long.Parse(Console.ReadLine());
+            }
+            for (int i = 0; i < sajatMeresek.Length; i++)
+            {
+                Console.WriteLine(sajatMeresek[i]);
+            }
+                
+
+        }
+    }
+}
